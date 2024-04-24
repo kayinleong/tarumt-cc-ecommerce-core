31e9ec6afba87f0e8013b20cd35fe08c600f37d5
Kayin <34983642+kayinleong@users.noreply.github.com>
2024-04-24 09:04:27 +0800

    Update file

--- Tarumt.CC.Ecommerce.Core/obj/Release/net8.0/Tarumt.CC.Ecommerce.Core.AssemblyInfo.cs
+++ Tarumt.CC.Ecommerce.Core/obj/Release/net8.0/Tarumt.CC.Ecommerce.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarumt.CC.Ecommerce.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2894e867c0ce3b44832febe76ce2b91dbc0328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba832d5c9f571fc8ab176469c8aae2cf3eb3da7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarumt.CC.Ecommerce.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarumt.CC.Ecommerce.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
